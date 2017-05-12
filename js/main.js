a8e2c9b42ba5b8a0c209a90a7957121e5fbbf02c
defremont <andre.def93@gmail.com>
2017-05-11 21:21:45 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-// Dean Attali / Beautiful Jekyll 2016
-
 var main = {
 
   bigImgEl : null,
@@ -133,6 +131,5 @@ var main = {
   }
 };
 
-// 2fc73a3a967e97599c9763d05e564189
 
 document.addEventListener('DOMContentLoaded', main.init);
